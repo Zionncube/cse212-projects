5a25c8fe654847bddf2117f9bb0ae3507c9e4858
Zionncube <mercy.msawenkosi@gmail.com>
2025-07-17 16:40:10 +0200

    Update FeatureCollection.cs

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,18 @@
 public class FeatureCollection
 {
-    // TODO Problem 5 - ADD YOUR CODE HERE
-    // Create additional classes as necessary
+    // List of earthquake features from the JSON
+    public List<Feature> Features { get; set; }
+}
+
+// Represents each feature in the list
+public class Feature
+{
+    public Properties Properties { get; set; }
+}
+
+// Holds the location and magnitude of the earthquake
+public class Properties
+{
+    public string Place { get; set; }      // Location description
+    public double? Mag { get; set; }       // Magnitude
 }
