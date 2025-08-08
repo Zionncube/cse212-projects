be6e5fd77e0fa3a2cd942d1d5f938272f7686dea
Zionncube <mercy.msawenkosi@gmail.com>
2025-08-08 10:00:32 +0200

    Update Node.cs

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,45 +1,62 @@
+/// <summary>
+/// Represents a single node in the binary search tree.
+/// </summary>
 public class Node
 {
-    public int Data { get; set; }
-    public Node? Right { get; private set; }
-    public Node? Left { get; private set; }
+    public int Data { get; set; }        // The data stored in the node
+    public Node? Left { get; set; }      // Pointer to the left child
+    public Node? Right { get; set; }     // Pointer to the right child
 
     public Node(int data)
     {
-        this.Data = data;
+        Data = data;
     }
 
+    /// <summary>
+    /// Inserts a new value into the subtree starting from this node.
+    /// </summary>
     public void Insert(int value)
     {
-        // TODO Start Problem 1
-
         if (value < Data)
         {
             // Insert to the left
-            if (Left is null)
+            if (Left == null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else
+        else if (value > Data)
         {
             // Insert to the right
-            if (Right is null)
+            if (Right == null)
                 Right = new Node(value);
             else
                 Right.Insert(value);
         }
+        // Duplicate values are ignored (not added again)
     }
 
+    /// <summary>
+    /// Searches for a value in the subtree.
+    /// </summary>
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+        else if (value < Data)
+            return Left != null && Left.Contains(value);
+        else
+            return Right != null && Right.Contains(value);
     }
 
+    /// <summary>
+    /// Calculates the height of the subtree rooted at this node.
+    /// </summary>
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
